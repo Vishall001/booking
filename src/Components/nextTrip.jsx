cf67bc734e6204bf301648db81bd8d608ebbd7b9
Vishall001 <vshmandloi@gmail.com>
2022-08-26 15:41:55 +0530

    some changes

--- src/Components/nextTrip.jsx
+++ src/Components/nextTrip.jsx
@@ -28,7 +28,7 @@ export const Nextrip = () => {
             backgroundRepeat={"no-repeat"}
             backgroundImage='url("https://cf.bstatic.com/xdata/images/xphoto/540x405/165922442.webp?k=7620df87dcc9ef0cd2fe66423dbf11fbdc2cd17e090856d8046aa3ec60dd3764&amp;o=");'
           >
-            <Box position={"absolute"} bottom="2" right="0" textAlign={"start"}>
+            <Box position={"absolute"} bottom="2" right="0" left="5" textAlign={"start"}>
               <Text
                 color={"white"}
                 fontSize={["12px", "15px", "18px", "20px"]}
@@ -38,7 +38,7 @@ export const Nextrip = () => {
               </Text>
               <Text color={"white"} fontWeight={"500"}>
                 The best place in europe for an idyllic late-summer escape with
-                friends.{" "}
+                friends.
               </Text>
             </Box>
           </Box>
@@ -50,7 +50,7 @@ export const Nextrip = () => {
             backgroundRepeat={"no-repeat"}
             backgroundImage='url("https://cf.bstatic.com/xdata/images/xphoto/720x405/165609232.webp?k=4367ee64da98f68300d41a1c85ce3b2fe518b4c046cb2021d7bc34a499ace5dd&amp;o=");'
           >
-            <Box position={"absolute"} bottom="2" right="0" textAlign={"start"}>
+            <Box position={"absolute"} bottom="2" right="0" left="5" textAlign={"start"}>
               <Text
                 color={"white"}
                 fontSize={["12px", "15px", "18px", "20px"]}
@@ -73,7 +73,7 @@ export const Nextrip = () => {
             backgroundPosition={"center"}
             backgroundImage='url("https://cf.bstatic.com/xdata/images/xphoto/540x405/167587831.webp?k=31e6256fc044ff9b127babfafcde346ded92aa66545f925fa84b749a8226dc30&amp;o=")'
           >
-            <Box position={"absolute"} bottom="2" right="0" textAlign={"start"}>
+            <Box position={"absolute"} bottom="2" right="0" left="5" textAlign={"start"}>
               <Text
                 color={"white"}
                 fontSize={["12px", "15px", "18px", "20px"]}
@@ -96,7 +96,7 @@ export const Nextrip = () => {
             backgroundPosition={"center"}
             backgroundImage='url("https://cf.bstatic.com/xdata/images/xphoto/540x405/167616822.webp?k=31bae8840b12db441ce925b86f3f5149fda3f9851de6dfbd48cfff89a6680b74&amp;o=")'
           >
-            <Box position={"absolute"} bottom="2" right="0" textAlign={"start"}>
+            <Box position={"absolute"} bottom="2" right="0" left="5" textAlign={"start"}>
               <Text
                 color={"white"}
                 fontSize={["12px", "15px", "18px", "20px"]}
@@ -119,7 +119,7 @@ export const Nextrip = () => {
             backgroundPosition={"center"}
             backgroundImage='url("https://cf.bstatic.com/xdata/images/xphoto/540x405/167580228.webp?k=081bfa5713fa309507294247377ac4d4b9c23d45bbb3161711c97ba2847b7bfc&amp;o=");'
           >
-            <Box position={"absolute"} bottom="2" right="0">
+            <Box position={"absolute"} bottom="2" right="0" left="5">
               <Text
                 textAlign={"start"}
                 color={"white"}
